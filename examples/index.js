fa329a67eeb58588380217ab6cce3dc572ed685b
gengwenhao <gengwenhao97@126.com>
2024-02-25 02:15:22 +0800

    chore: move dev

--- examples/index.js
+++
@@ -1,8 +0,0 @@
-import bootstrap from 'geng-web/src/core.js'
-import {success} from 'geng-web/src/utils/debug.js'
-import routes from './routes.js'
-
-bootstrap('localhost', 3000, routes)
-  .then(({host, port}) => {
-    success(`server is running on http://${host}:${port}`)
-  })
